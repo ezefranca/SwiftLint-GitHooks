eb2853ae3d0ec93cf49892bc98a480a4fabc37eb
Ezequiel Santos <ext.ezequiel.santos@farfetch.com>
2022-05-19 08:12:46 +0100

    Initial Commit

---
+++ SwiftLint-GitHooks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftLint-GitHooks
+//
+//  Created by Ezequiel on 19/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftLint-GitHooks/SwiftLint_GitHooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftLint_GitHooksApp.swift
+//  SwiftLint-GitHooks
+//
+//  Created by Ezequiel on 19/05/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftLint_GitHooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
